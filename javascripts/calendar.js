170edd42d945d6e261a2577ddc7a4119778365d8
Tsvetan Tsvetkov <tsekach@gmail.com>
2012-08-14 21:51:59 +0300

    Set-up project page

---
+++ javascripts/calendar.js
@@ -0,0 +1,97 @@
+angular.module('Calendar', [])
+	.directive('searchfilter', function() {
+		return {
+			controller: function() {},
+			restrict: 'E',
+			templateUrl: 'templates/searchFilter.html',
+			replace: true,
+			link: function(scope, element, attrs, controller) {
+				scope.searchText = "";
+				scope.searchFilterSelect = "subject";
+			}
+		}
+	})
+	.directive('calendar', function() {
+		return {
+			controller: function($scope) {
+				//Available locates current locate
+				$scope.locales = ["bg", "en-gb", "jp", "ko"];
+				$scope.lang = $scope.locales[0];
+
+				//Set current date
+				$scope.date = new Date;
+
+				$scope.events = [];
+
+				//Get localized months' names
+				$scope.months = function () {
+					return moment.months;
+				}
+			},
+			restrict: 'E',
+			templateUrl: 'templates/calendar.html',
+			replace: true,
+
+			link: function(scope, element, attrs, controller) {
+				//adding new event flag - used to toggle newEvent page
+				scope.newEvent = false;
+
+				/* public method */
+				scope.toggleNewEventPage = function(addNew) {
+					scope.newEventSubject = "";
+					scope.newEventLocation = "";
+
+					scope.newEvent = !!addNew;
+				}
+
+				//Change date event listener
+				scope.changeDate = function(step) {
+					var date = scope.date.getDate();
+
+					scope.date.setDate(date + step);
+				}
+
+				//Set date today event listener
+				scope.today = function() {
+					scope.date = new Date;
+				}
+
+				//Add new event to the list
+				scope.addNew = function() {
+					scope.events.push({
+						subject: scope.newEventSubject,
+						location: scope.newEventLocation,
+						date: (new Date).getTime()
+					})
+					scope.toggleNewEventPage(false);
+				}
+
+				//Change locale listener. Sets moment.js with selected locale
+				scope.changeLocale = function() {
+					moment.lang(scope.lang);
+				}
+
+				//Filtering events function. 
+				//Filters events based on the selected filter and entered text
+				scope.filteredEvents = function(element, element2) {
+					return function(element) {
+						return element[scope.searchFilterSelect].toLowerCase()
+								.match((scope.searchText || "").toLowerCase());
+					};
+				}
+
+				//set initial locale
+				scope.changeLocale();
+			}
+		}
+	})
+	.filter('dateFilter', function() {
+		return function(input, format) {
+			return moment(input).format(format);
+		}
+	})
+	.filter("eventsFilter", function() {
+		return function(input, format) {
+			return input
+		}
+	});
